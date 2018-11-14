bc57325e215d7ee1374c8d2f5914f4a9cae3b6ee
AntonioLagej <junior.alpv@gmail.com>
2018-11-13 23:42:55 -0200

    Create biblioteca.h

---
+++ biblioteca.h
@@ -0,0 +1,38 @@
+/*
+ * biblioteca.h
+ *
+ *  Created on: 13 de nov de 2018
+ *      Author: Junior
+ */
+
+#ifndef BIBLIOTECA_H_
+#define BIBLIOTECA_H_
+
+#define CODIGO(x) (x*100+1);
+typedef struct CARRO EstruturaCarro;
+typedef struct PESSOA EstruturaPessoa;
+int verificarData(char data[]);
+struct CARRO {
+	char placa[7];
+	int codigo;
+	char modelo[50];
+	int ano;
+	EstruturaCarro *anterior;
+};
+
+struct PESSOA {
+	char nome[50];
+	int codigo;
+	char dataNasc[10];
+	char contrato;
+	int qtdPlaca;
+	EstruturaCarro *dadosDosCarros;
+
+	EstruturaPessoa *proximo;
+	EstruturaPessoa *anterior;
+};
+void menu();
+int concatINT(char s[], int inicio, int fim);
+void cadastroPessoa(EstruturaPessoa *lista);
+int cadastroCarro(EstruturaCarro *lista);
+#endif /* BIBLIOTECA_H_ */
